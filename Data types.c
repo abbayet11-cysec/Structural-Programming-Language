b430698882cf7467d137e4b2d6c9cc84a952edae
abbayet11-cysec <abbayet11@gmail.com>
2026-09-08 15:16:26 +0600

    Add files via upload

---
+++ Data types.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+    int main() {
+    int studentID=502;
+    float score=88.88;
+    char grade='Z';
+
+printf("ID=%d\nGot %f marks and secured %c grade!", studentID, score, grade);
+
+return 0;
+}
