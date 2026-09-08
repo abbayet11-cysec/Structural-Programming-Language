d4696115592b91fd30257d02c69475a7018cfa0d
abbayet11-cysec <abbayet11@gmail.com>
2026-09-08 15:37:48 +0600

    Add files via upload

---
+++ Operators.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+    int main() {
+    int itemprice=60,shippingcost=130,sum;
+
+sum=itemprice+shippingcost;
+
+printf(" Your item price is=%d\n shipping cost will be=%d\n total cost is =%d BDT\n sorry but khajnar cheye bajna beshi",itemprice,shippingcost, sum); //fact:Dhakar baire delivery
+
+return 0;
+}
